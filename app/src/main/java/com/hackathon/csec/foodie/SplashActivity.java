5d31a61cf4f52d7c44c7388a3408aa0b4feeff07
palak97 <palaksood1997@gmail.com>
2017-02-04 11:43:27 -0800

    Added Splash Activity

---
+++ app/src/main/java/com/hackathon/csec/foodie/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.hackathon.csec.foodie;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.target.Target;
+
+public class SplashActivity extends AppCompatActivity {
+private ImageView image_splash;
+private static  final int TIME_SPLASH=1500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        image_splash = (ImageView) findViewById(R.id.image_splash);
+        Target<GlideDrawable> into = Glide.with(SplashActivity.this).load(R.drawable.splash).into(image_splash);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },TIME_SPLASH);
+
+    }
+
+}
+
+
