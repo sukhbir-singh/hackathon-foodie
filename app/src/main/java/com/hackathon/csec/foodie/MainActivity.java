279852c4eeb7012b50a7464915bc7fbae1bf6702
sukhbir-singh <sukhbir947@gmail.com>
2017-02-04 21:30:30 +0530

    main activity complete

--- app/src/main/java/com/hackathon/csec/foodie/MainActivity.java
+++ app/src/main/java/com/hackathon/csec/foodie/MainActivity.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.hackathon.csec.foodie.Adapter.HomeRecyclerAdapter;
+import com.hackathon.csec.foodie.AndroidModels.TrendingItems;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Foodie");
 
         recyclerView=(RecyclerView)findViewById(R.id.home_recycler);
+        HomeRecyclerAdapter adapter=new HomeRecyclerAdapter(this,getList());
 
-        LinearLayoutManager manager=new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
-
-        recyclerView.setLayoutManager(manager);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setAdapter(adapter);
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
 
@@ -37,25 +44,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(@IdRes int tabId) {
 
-            }
-        });
-
-        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelected(@IdRes int tabId) {
+                Log.v("weird","execution");
 
                 if (tabId == R.id.restaurant_tab) {
-
+                    //startActivity(new Intent(MainActivity.this,Restaurants_list.class));
 
                 }else if(tabId==R.id.cart_tab){
 
 
                 }else if(tabId==R.id.account_tab){
-                    
+
 
                 }
             }
         });
 
     }
+
+    public ArrayList<TrendingItems> getList(){
+      ArrayList<TrendingItems> items=new ArrayList<>();
+
+        String[] urls={"","","","","","","",""};
+        String[] titles={"Buy 1 Get 1 Free",
+                "50% OFF for First Time Customer",
+                "Flat 20% Off On Orders Above Rs.400",
+                "Flat Rs.75 Off On Order Of Rs.299",
+                "Free Burger On Order Above Rs.319",
+                "Free Meal On All Orders Above Rs.369",
+                "Overloaded Big Pizza Starting At Just Rs.219",
+                "Flat Rs.101 Off On Rs.400 & Above"};
+        String[] restaurants={"PIZZARO",
+                "DESI TADKA",
+                "KFC",
+                "CHOPRA",
+                "SURAJ",
+                "Mcdonalds",
+                "PIZZAHUT",
+                "DOMINOES"};
+
+        for(int i=0;i<restaurants.length;i++){
+            TrendingItems item=new TrendingItems();
+            item.setText(titles[i]);
+            item.setRestaurant(restaurants[i]);
+            item.setUrl(urls[i]);
+
+            items.add(item);
+        }
+
+        return items;
+    }
+
 }
