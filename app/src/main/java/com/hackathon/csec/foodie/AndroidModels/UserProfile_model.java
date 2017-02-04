94b49511976b08b12cd0377484e5a1dc02ac972e
TarunMahawar <tarunmahawar25@gmail.com>
2017-02-04 23:09:32 +0530

    User Profile in progress

---
+++ app/src/main/java/com/hackathon/csec/foodie/AndroidModels/UserProfile_model.java
@@ -0,0 +1,73 @@
+package com.hackathon.csec.foodie.AndroidModels;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+/**
+ * Created by TARUN on 04-Feb-17.
+ */
+
+public class UserProfile_model {
+
+    @SerializedName("name")
+    String nameUser;
+    @SerializedName("phoneno")
+    String phoneUser;
+    @SerializedName("email")
+    String emailuser;
+    @SerializedName("picUrl")
+    String picUrl;
+    @SerializedName("success")
+    String success;
+    @SerializedName("msg")
+    String msg;
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getPhoneUser() {
+        return phoneUser;
+    }
+
+    public void setPhoneUser(String phoneUser) {
+        this.phoneUser = phoneUser;
+    }
+
+    public String getEmailuser() {
+        return emailuser;
+    }
+
+    public void setEmailuser(String emailuser) {
+        this.emailuser = emailuser;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ app/src/main/java/com/hackathon/csec/foodie/UserProfile.java
@@ -0,0 +1,63 @@
+package com.hackathon.csec.foodie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hackathon.csec.foodie.AndroidModels.Restaurant_model;
+import com.hackathon.csec.foodie.AndroidModels.UserProfile_model;
+import com.hackathon.csec.foodie.Utilities.APIINTERFACE;
+import com.hackathon.csec.foodie.Utilities.Utils;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.R.id.list;
+
+public class UserProfile extends AppCompatActivity {
+
+    TextView nameUser, emailUser, phoneUser;
+
+    String picUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        nameUser = (TextView)findViewById(R.id.nameUser);
+        phoneUser= (TextView)findViewById(R.id.phoneUser);
+        emailUser= (TextView)findViewById(R.id.emailUser);
+
+        retrofit();
+
+    }
+
+    public void retrofit(){
+
+        APIINTERFACE apiservice= Utils.getRetrofitService();
+        Call<UserProfile_model> call=apiservice.getUserInfo();
+
+        call.enqueue(new Callback<UserProfile_model>() {
+            @Override
+            public void onResponse(Call<UserProfile_model> call, Response<UserProfile_model> response) {
+                bar.setVisibility(View.GONE);
+
+                Restaurant_model model=response.body();
+                int status=response.code();
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<UserProfile_model> call, Throwable t) {
+                bar.setVisibility(View.GONE);
+                Toast.makeText(UserProfile.this,"Some error occurred!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
